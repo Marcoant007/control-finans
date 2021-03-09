5bae8e82745c0ca8b6a915a8aa96e23d115bea8f
Marcoant007 <marcoantnovo@gmail.com>
2021-03-09 11:23:07 -0300

    Iniciando frontend

--- src/services/Users/CreateUserService.ts
+++ src/services/Users/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getCustomRepository } from "typeorm";
 import User from "../../models/User";
 import UserRepository from "../../repositories/UsersRepository";
+import { hash } from 'bcryptjs'
+
 
 interface Request {
     name: string,
@@ -10,14 +12,23 @@ interface Request {
 }
 
 class CreateUserService {
-    public async execute({name, email, password, salary}:Request):Promise<User>{
-        const userRepository = getCustomRepository(UserRepository)
+    public async execute({ name, email, password, salary }: Request): Promise<User> {
+        const userRepository = getCustomRepository(UserRepository);
+        const checkUserExist = await userRepository.findOne({
+            where: {email}
+        })
+
+        if(checkUserExist){
+            throw new Error('Email ja existe');
+        }
+        const hashedPassword = await hash(password, 8);
         const userCreate = userRepository.create({
             name,
             email,
-            password,
+            password: hashedPassword,
             salary
-        })
+        });
+
         await userRepository.save(userCreate)
         return userCreate
     }
